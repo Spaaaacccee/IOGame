dad090b3b0b92c4149ff215bbba4f4962326b035
Spaaaacccee <kaiwen530@gmail.com>
2018-04-12 00:41:54 +1000

    Tidied code and added source mapping

--- gulpfile.js
+++ gulpfile.js
@@ -8,62 +8,62 @@ const sourcemaps = require('gulp-sourcemaps');
 const colors = require('colors');
 
 const bundleTask = function () {
-	gulp.src('src/entry.js').pipe(webpack({
-		devtool: 'source-map',
-		module: {
-			rules: [{
-				test: /\.css$/,
-				use: [
-					{
-						loader: "babel-loader",
-						options: {
-							presets: ['@babel/preset-env']
-						}
-					},
-					{
-						loader: "style-loader"
-					},
-					{
-						loader: "css-loader"
-					},
-				]
-			}]
-		},
-		output: {
-			filename: 'bundle.js'
-		}
-	})).pipe(gulp.dest('static/js/')).on('end', function () {
-		console.log('[Webpack] Compiled'.cyan);
-	});
+    gulp.src('src/entry.js').pipe(webpack({
+        devtool: 'source-map',
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    },
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                ]
+            }]
+        },
+        output: {
+            filename: 'bundle.js'
+        }
+    })).pipe(gulp.dest('static/js/')).on('end', function () {
+        console.log('[Webpack] Compiled'.cyan);
+    });
 
-}
+};
 
 gulp.task('default', ['browser-sync'], function () {});
 
 gulp.task('browser-sync', ['nodemon'], function () {
-	browserSync.init(null, {
-		proxy: "http://localhost:5000",
-		files: ["static/**/*.*"],
-		browser: "chrome",
-		port: 7000,
-	});
+    browserSync.init(null, {
+        proxy: 'http://localhost:5000',
+        files: ['static/**/*.*'],
+        browser: 'chrome',
+        port: 7000,
+    });
 });
 
 gulp.task('nodemon', function (cb) {
 
-	var started = false;
+    var started = false;
 
-	bundleTask();
+    bundleTask();
 
-	return nodemon({
-		script: 'index.js',
-		ignore: ['./.vscode/', './src/', './static/', 'gulpfile.js']
-	}).on('start', function () {
-		if (!started) {
-			cb();
-			started = true;
-		}
-	}).on('restart', function () {
-		bundleTask();
-	});
+    return nodemon({
+        script: 'index.js',
+        ignore: ['./.vscode/', './src/', './static/', 'gulpfile.js']
+    }).on('start', function () {
+        if (!started) {
+            cb();
+            started = true;
+        }
+    }).on('restart', function () {
+        bundleTask();
+    });
 });
